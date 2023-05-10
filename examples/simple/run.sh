9f44c04c3653c1ce6bfd211a15b3f0dc2a927e86
Phil Budne <pbudne@angwin.cs.umass.edu>
2023-05-10 01:12:36 +0000

    add comment to examples/simple/run.sh

--- examples/simple/run.sh
+++ examples/simple/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# run this from top level (stories-pipeline) directory,
+# after creating a virtual environment, activating, and
+# installing requirements.
+
 rm -rf /tmp/simple
 mkdir /tmp/simple
 python -m scripts.configure -f examples/simple/plumbing.json configure
